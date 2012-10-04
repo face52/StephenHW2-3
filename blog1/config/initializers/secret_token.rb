5298e918bd7d2548dd2f3825f30f5066e54b874a
face52 <face52@unm.edu>
2012-10-03 23:52:50 -0600

    homework2-3

---
+++ blog1/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog1::Application.config.secret_token = '36ae9768a58ca65beee4f56b51341a1b67654ea0d815c60ac422cd5f7ff7d49f6057cfd1fa3a7edb1f61b818ccb029a2492794f6f37a49b4626ba9fcb28a445d'
